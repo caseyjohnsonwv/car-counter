25511b614f932c6251fcfc2483c7cac658af1957
Casey Johnson <ctj0001@mix.wvu.edu>
2019-11-08 01:21:17 -0500

    app.run()

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, redirect, session, request, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET"])
 #main page with statistics
@@ -9,12 +11,20 @@ def hello():
     app.config.update(SECRET_KEY = os.urandom(24))
     return "Hello, world!"
 
+
 @app.route("/alive", methods=["GET"])
 #simple get request to test wi-fi connectivity
 def alive():
     return "Success"
 
+
 @app.route("/upload-data", methods=["POST"])
 #route for HTTP POST request
 def upload_data():
     return "Success"
+
+
+if __name__ == "__main__":
+    host = os.environ.get("host","127.0.0.1")
+    port = os.environ.get("port", "5000")
+    app.run(host,port)
